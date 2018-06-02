3acb4cd0a3a308f94e6f312bc9b6b7f5df37ba31
Rajasekhar Matam <sekhar.matam@gmail.com>
2018-06-02 16:32:04 +0530

    Add files via upload

---
+++ dependency-UDPipe-shinyApp.R
@@ -0,0 +1,20 @@
+# setup code block
+try(require(shiny) || install.packages("shiny"))
+if (!require(udpipe)){install.packages("udpipe")}
+if (!require(textrank)){install.packages("textrank")}
+if (!require(lattice)){install.packages("lattice")}
+if (!require(igraph)){install.packages("igraph")}
+if (!require(ggraph)){install.packages("ggraph")}
+if (!require(wordcloud)){install.packages("wordcloud")}
+try(require("fmsb")||install.packages("fmsb"))
+
+library("shiny")
+library("fmsb")
+library(udpipe)
+library(textrank)
+library(lattice)
+library(igraph)
+library(ggraph)
+library(ggplot2)
+library(wordcloud)
+library(stringr)
